d34a43eecf1466db935f849d65a4b14cef69ebd5
ParagChandraRai <80310600+ParagChandraRai@users.noreply.github.com>
2023-04-27 16:36:30 +0530

    Add files via upload

---
+++ SpeakSimplified.js
@@ -0,0 +1,22 @@
+var s=document.getElementById("txt");
+function exclusion()
+{
+ var data= s.value;
+ data="un"+data;
+ document.getElementById("output").value=data;
+}
+function  strengthen ()
+{
+  
+ var data= s.value;
+ data="Plus"+data;
+ document.getElementById("output").value=data;
+}
+function emphasize()
+{
+    
+ var data= s.value;
+ data="DoublePlus"+data;
+ document.getElementById("output").value=data;
+
+}
